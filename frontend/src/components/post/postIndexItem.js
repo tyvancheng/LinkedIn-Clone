7e7e3935d34534f0a54ef0f42c780ded5eacfd85
Tyvan Cheng <tyvancheng@gmail.com>
2023-06-26 10:35:26 -0400

    added some comments

--- frontend/src/components/post/postIndexItem.js
+++ frontend/src/components/post/postIndexItem.js
@@ -132,7 +132,7 @@ export default function PostIndexItem({ post }) {
                 <button>{<img src={sendIcon} alt='sendicon' />}Share</button>
             </div>
 
-            {/* Comment Section of a Post*/}
+            {/* Comment Section */}
             {(commentLog === true) && (
                 <>
                     <input value={commentValue} onChange={handleCommentChange} placeholder='Add a comment...'></input>

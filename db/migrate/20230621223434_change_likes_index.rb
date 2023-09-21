7f724bf8826376014055cc309d240c2bf0f759c1
Tyvan Cheng <tyvancheng@gmail.com>
2023-09-21 17:28:44 -0400

    removed remove index on likes

--- db/migrate/20230621223434_change_likes_index.rb
+++ db/migrate/20230621223434_change_likes_index.rb
@@ -1,6 +1,6 @@
 class ChangeLikesIndex < ActiveRecord::Migration[7.0]
   def change
-    remove_index :likes, :liker_id
+    # remove_index :likes, :liker_id
     add_index :likes, [:liker_id, :post_id], unique: true
   end
 end
